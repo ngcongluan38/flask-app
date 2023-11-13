b2eb776cc1eae0a97ea6a1d79aec6430ef3accf9
ngcongluan38 <138641335+ngcongluan38@users.noreply.github.com>
2023-11-13 17:32:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -62,5 +62,5 @@ def download_file():
     return send_file(processed_path, as_attachment=True)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(debug=True, port=port) 
